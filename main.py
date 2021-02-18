2b3cedcd2ef59cfeea9611c8389086043be65ef6
Kronifer <dillonr5@live.wsd1.org>
2021-02-18 19:05:58 +0000

    added search by genre

--- main.py
+++ main.py
@@ -11,30 +11,53 @@ def home():
 
 @app.route('/api/v1/data/add', methods=["GET"])
 def adddata():
-    title = request.args.get("title")
-    author = request.args.get("author")
-    genre = request.args.get("genre")
+    title = request.args.get("title").lower()
+    author = request.args.get("author").lower()
+    genre = request.args.get("genre").lower()
     try:
         db[title]
         return "Book has already been added."
     except:
         db[title] = title
-        db[title + "a"] = author
-        db[title + "g"] = genre
+        db[title + "@"] = author
+        db[title + "^"] = genre
         return ("Data has been added!")
 
 
 @app.route('/api/v1/data/get', methods=["GET"])
 def getdata():
-    title = request.args.get("title")
+    title = request.args.get("title").lower()
     tvalue = db[title]
-    gvalue = db[title + 'g']
-    avalue = db[title + 'a']
+    gvalue = db[title + '^']
+    avalue = db[title + '@']
     data = {}
     data.update({"title": tvalue})
     data.update({"author": avalue})
     data.update({"genre": gvalue})
     return jsonify(data)
+
+@app.route('/api/v1/data/getgenre', methods=["GET"])
+def getbygenre():
+    genre = request.args.get('genre').lower()
+    keys = db.keys()
+    data = {}
+    for key in keys:
+        newtitle = ""
+        value = db[key]
+        badchars = ["@", "^"]
+        if value == genre:
+            for element in key:
+                if element in badchars:
+                    pass
+                else:
+                    newtitle += element
+            data.update({newtitle: value})
+        else:
+            pass
+    return jsonify(data)
+            
+
+
 
 
 app.run(host="0.0.0.0", port="8080")
