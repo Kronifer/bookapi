ade6aa2b7cf2a6fd08e17c40d2595c5bcb532c04
Dillon Runke <dillonr5@live.wsd1.org>
2021-02-24 17:01:17 +0000

    error fix

--- main.py
+++ main.py
@@ -1,67 +1,90 @@
 from flask import Flask, request, jsonify, render_template
 import pymongo
+import os
+import time
+from threading import Thread
+import sys
 
-app = Flask(__name__)
+def main():
 
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb+srv://APIUSER:book_api@books.1zhnr.mongodb.net/Bookapi?retryWrites=true&w=majority")
-db = client.Bookapi
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-@app.route('/', methods=["GET"])
-def home():
-    return render_template("home.html")
+    client = \
+        pymongo.MongoClient('mongodb+srv://APIUSER:book_api@books.1zhnr.mongodb.net/Bookapi?retryWrites=true&w=majority'
+                            )
+    db = client.Bookapi
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return render_template('home.html')
+
+    @app.route('/api/v1/data/add', methods=['GET'])
+    def adddata():
+        title = request.args.get('title').lower()
+        author = request.args.get('author').lower()
+        genre = request.args.get('genre').lower()
+        publisher = request.args.get('publisher').lower()
+        yearpublished = int(request.args.get('pubyear'))
+        data = {}
+        data.update({'title': title})
+        data.update({'author': author})
+        data.update({'genre': genre})
+        data.update({'publisher': publisher})
+        data.update({'year published': yearpublished})
+        db.Books.insert_one(data)
+        return 'Data has been added!'
+
+    @app.route('/api/v1/data/get', methods=['GET'])
+    def getdata():
+        title = request.args.get('title')
+        collection = db.Books
+        doc = collection.find_one({'title': title}, {'_id': 0})
+        if doc == None:
+            return 'Book is not registered.'
+        else:
+            return jsonify(doc)
+
+    @app.route('/api/v1/data/getbygenre', methods=['GET'])
+    def getbygenre():
+        genre = request.args.get('genre').lower()
+        collection = db.Books
+        docs = collection.find({'genre': genre}, {'_id': 0})
+        data = []
+        for document in docs:
+            data.append(document)
+        return jsonify(data)
+
+    @app.route('/about', methods=['GET'])
+    def about():
+        return render_template('about.html')
+    
+    @app.route("/admin/shutdown", methods=["GET"])
+    def shutdown_server():
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+
+    @app.errorhandler(404)
+    def error404(e):
+        return render_template('404.html')
+    
+    
+
+    def run():
+        app.run(host='0.0.0.0', port='8080')
+
+    apiserver = Thread(target=run)
+    apiserver.start()
 
 
-@app.route('/api/v1/data/add', methods=["GET"])
-def adddata():
-    title = request.args.get("title").lower()
-    author = request.args.get("author").lower()
-    genre = request.args.get("genre").lower()
-    publisher = request.args.get("publisher").lower()
-    yearpublished = int(request.args.get("pubyear"))
-    data = {}
-    data.update({"title": title})
-    data.update({"author": author})
-    data.update({"genre": genre})
-    data.update({"publisher": publisher})
-    data.update({"year published": yearpublished})
-    db.Books.insert_one(data)
-    return "Data has been added!"
-
-
-@app.route('/api/v1/data/get', methods=["GET"])
-def getdata():
-    title = request.args.get('title')
-    collection = db.Books
-    doc = collection.find_one({"title": title}, {"_id": 0})
-    if doc == None:
-        return "Book is not registered."
+if __name__ == "__main__":
+    if sys.argv[0] == 'test':
+        main()
+        time.sleep(3)
+        os.system('curl http://localhost:8080/api/v1/data/get?title=dragonwatch')
+        os.system("curl http://localhost:8080/admin/shutdown")
     else:
-        return jsonify(doc)
-
-
-
-@app.route('/api/v1/data/getbygenre', methods=["GET"])
-def getbygenre():
-    genre = request.args.get('genre').lower()
-    collection = db.Books
-    docs = collection.find({"genre": genre}, {"_id": 0})
-    data = []
-    for document in docs:
-        data.append(document)
-    return jsonify(data)
-
-
-
-@app.route('/about', methods=["GET"])
-def about():
-    return render_template("about.html")
-
-
-@app.errorhandler(404)
-def error404(e):
-    return render_template("404.html")
-
-
-app.run(host="0.0.0.0", port="8080")
+        main()
