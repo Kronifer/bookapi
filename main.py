754197060e039aeff116509ada7781cbd2778a4e
Dillon Runke <dillonr5@live.wsd1.org>
2021-03-23 15:00:04 +0000

    moved to .env

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
     client = pymongo.MongoClient(
-        "mongodb+srv://APIUSER:book_api@books.1zhnr.mongodb.net/Bookapi?retryWrites=true&w=majority"
+        os.getenv("MKEY")
     )
 
     db = client.Bookapi
